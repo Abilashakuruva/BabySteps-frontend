9c8d5d17c2e0a91a1602eeaddf9d482a951375c6
Abilashakuruva <kuruvaabilasha218@gmail.com>
2025-02-24 18:56:04 +0530

    added code

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { bookAppointment } from "../api/api";
+import moment from "moment";
 
 const AppointmentForm = ({ doctorId, slot }) => {
   const [patientName, setPatientName] = useState("");
@@ -7,17 +8,26 @@ const AppointmentForm = ({ doctorId, slot }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const date = moment().format("YYYY-MM-DD"); // Get today's date
+    const formattedDate = moment(`${date} ${slot}`, "YYYY-MM-DD HH:mm").toISOString();
+
+
     const appointmentData = {
       doctorId,
-      date: slot,
+      date: formattedDate,
       duration: 30,
       appointmentType,
       patientName,
     };
+
+
+
     try {
       await bookAppointment(appointmentData);
       alert("Appointment booked!");
     } catch (error) {
+      console.error("Error booking appointment:", error);
       alert("Error booking appointment");
     }
   };
@@ -29,6 +39,13 @@ const AppointmentForm = ({ doctorId, slot }) => {
       <select value={appointmentType} onChange={(e) => setAppointmentType(e.target.value)}>
         <option value="Routine Check-Up">Routine Check-Up</option>
         <option value="Ultrasound">Ultrasound</option>
+        <option value="Scanning">Scanning</option>
+        <option value="Thyroid">Thyroid</option>
+        <option value=" Abdominal Pregnancy Ultrasound">Abdominal Pregnancy Ultrasound</option>
+        <option value="Bone fracture Ultrasound">Bone fracture Ultrasound</option>
+        <option value="Care & Services">Care & Services</option>
+        <option value="Early Pregnancy Tips">Early Pregnancy Tips</option>
+        <option value="ENT">ENT</option>
       </select>
       <button type="submit">Book</button>
     </form>
